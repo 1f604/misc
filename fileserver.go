17d7ab8a99bdb0587a304a2c995aa822cb189f29
1f604 <1f604@users.noreply.github.com>
2021-06-06 18:08:32 +0100

    Create fileserver.go

---
+++ fileserver.go
@@ -0,0 +1,139 @@
+// Simple file server using HTTP basic authentication
+// Allows 3 consecutive failed logins (configurable), after which all requests will be denied and server must be restarted.
+// Make sure you change the configuration as specified below.
+// Place files in the subdirectory in the same directory where this file is.
+/* Default directory structure:
+   - httpserver.go (this file)
+   - filestobeserved (directory)
+      - file1.html (file)
+      - assets (directory)
+         - file2.jpg (file)
+*/
+package main
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"sync"
+)
+
+// Configuration
+var (
+	// Make sure you change these:
+	require_auth      = true
+	username          = "admin"
+	password          = "12345"
+	allowedsubdir     = "filestobeserved"
+	allowedsubsubdirs = [...]string{"/", "/assets", "/random"}
+	port              = "12345"
+	// You can leave these as they are.
+	max_failed_logins   = 3
+	print_failed_logins = true
+	realm               = "12345"
+	default_filename    = "index.html"
+)
+
+type counter struct {
+	mu sync.Mutex
+	n  int
+}
+
+func (c *counter) inc() {
+	c.mu.Lock()
+	c.n++
+	c.mu.Unlock()
+}
+
+func (c *counter) get() int {
+	c.mu.Lock()
+	var n = c.n
+	c.mu.Unlock()
+	return n
+}
+
+func (c *counter) reset() {
+	c.mu.Lock()
+	c.n = 0
+	c.mu.Unlock()
+}
+
+var allowedDirectories = map[string]bool{}
+var failed_logins = counter{n: 0}
+
+func main() {
+	for _, element := range allowedsubsubdirs {
+		allowedDirectories[element] = true
+	}
+
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/", handler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// Have to do this because regular expressions in Go are insanely slow.
+func isValidFilename(s string) bool {
+	for _, r := range s {
+		if (r < 'a' || r > 'z') && (r < '0' || r > '9') && r != '.' && r != '_' {
+			return false
+		}
+	}
+	return true
+}
+
+func handleAuthorized(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Successful login attempt.")
+	failed_logins.reset()
+	filename := default_filename
+	requested := path.Base(r.URL.Path)
+	predir := path.Dir(r.URL.Path)
+	if _, ok := allowedDirectories[predir]; !ok {
+		fmt.Println("Invalid directory requested:" + predir)
+		w.WriteHeader(403)
+		return
+	} else {
+		if isValidFilename(requested) {
+			filename = requested
+		}
+		if !strings.HasSuffix(predir, "/") {
+			predir += "/"
+		}
+		filepath := allowedsubdir + predir + filename
+		fmt.Println("Serving file: " + filepath)
+		if _, err := os.Stat(filepath); os.IsNotExist(err) {
+			fmt.Println("Error: File " + filepath + " does not exist.")
+		}
+		http.ServeFile(w, r, filepath)
+	}
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if failed_logins.get() >= max_failed_logins {
+		w.WriteHeader(400)
+		return
+	}
+
+	if require_auth {
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			failed_logins.inc()
+			if print_failed_logins {
+				fmt.Println(`Failed login attempt used username: "` + user + `" and password: "` + pass + `"`)
+			}
+			if failed_logins.get() == max_failed_logins {
+				fmt.Println("Max failed login attempts reached. Please restart server.")
+			}
+			w.Header().Set("WWW-Authenticate", `Basic realm=`+realm)
+			w.WriteHeader(401)
+			return
+		}
+	}
+
+	handleAuthorized(w, r)
+	return
+}
