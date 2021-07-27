3c8a2277921545395c48c8d3e291c42296d3c0c4
1f604 <1f604@users.noreply.github.com>
2021-07-27 12:24:07 +0100

    Update util.py

--- util.py
+++ util.py
@@ -2,3 +2,11 @@
 
 def sort_date_strings(timestamps): # e.g. '2011-06-2', '2011-08-05', '2010-1-14', etc
   timestamps.sort(key=lambda date: datetime.datetime.strptime(date, "%Y-%m-%d"))
+  
+def sort_files(fileslist): # sort files or directories by modification time
+    results = []
+    for path in fileslist:
+        mtime = os.stat(path).st_mtime # make sure to give absolute paths to os.stat()
+        results.append((mtime, path))
+    results.sort()
+    return [path for mtime, path in results]
